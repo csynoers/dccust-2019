ef8db96dfc6683a0ce6dbd007f5e3b145161f872
csynoers <3s0c9m7@gmail.com>
2019-09-05 15:38:54 +0700

    halaman website detail artikel

--- joinc/detail/detail_publikasi.php
+++ joinc/detail/detail_publikasi.php
@@ -1,32 +1,33 @@
 	<section id="featured">
-	<!-- start slider -->
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-12">
-	<!-- Slider -->
-      <div id="main-slider" class="flexslider">
-            <ul class="slides">
-              <li>
-				<?php
-					$slide=mysql_query("SELECT * FROM header where id_header='6'");
-					$s=mysql_fetch_array($slide);
-					$nama	 	= $_SESSION['bahasa'] 	== "en" ? "nama_header_".$_SESSION[bahasa] : "nama_header_ina";
-					$isi	 	= $_SESSION['bahasa'] 	== "en" ? "isi_header_".$_SESSION[bahasa] : "isi_header_ina";
-				?>
-                <img src="joimg/header_image/<?php echo"$s[gambar]"; ?>" alt="" />
-                <div class="flex-caption">
-					
-                    <h3><?php echo"$s[$nama]"; ?></h3> 
-					<?php echo"$s[$isi]"; ?>
-                </div>
-              </li>
-            </ul>
-        </div>
-	<!-- end slider -->
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div id="main-slider" class="flexslider">
+							<ul class="slides">
+								<li>
+									<?php
+										$row= $db->get_select("SELECT * FROM header where id_header='6'")['data'][0];
+										echo '
+											<img src="joimg/header_image/'.$row->gambar.'" alt="" />
+											<div class="flex-caption">
+												
+												<h3>'.$row->nama_header_ina.'</h3> 
+												'.$row->isi_header_ina.'
+											</div>
+										';
+									?>
+								</li>
+							</ul>
+						</div>
+						<!-- /.flexslider -->
+				</div>
+				<!-- /.col -->
 			</div>
+			<!-- /.row -->
 		</div>
-	</div>	
+		<!-- /.container -->
 	</section>
+
 	<section id="featured">
 	<div class="container">
 		<div class="row">
@@ -37,24 +38,23 @@
 				</div>
 				<div class="row" style="margin-top: 19px; margin-bottom: 50px;"> 
 					<?php 
-						$publikasi=mysql_query("SELECT * FROM artikel where id_artikel='$_GET[id]'");
-						$f=mysql_fetch_array($publikasi);
-						
+						$row= $db->get_select("SELECT * FROM artikel where id_artikel='$_GET[id]'")['data'][0];
+						echo '
+							<article>
+								<div class="post-image">
+									<div class="post-heading">
+										<h3><a href="#">'.$row->nama_artikel.'</a></h3>
+										<!-- Go to www.addthis.com/dashboard to customize your tools -->
+											<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-54bc99eb04210058" async="async"></script>
+										<!-- Go to www.addthis.com/dashboard to customize your tools -->
+											<div class="addthis_sharing_toolbox"></div>
+									</div>
+									<img style="max-width: 70%" src="joimg/artikel/'.$row->gambar.'" alt="" />
+								</div>
+								'.$row->isi_artikel.'	
+							</article>					
+						';
 					?>
-					<article>
-						<div class="post-image">
-							<div class="post-heading">
-								<h3><a href="#"><?php echo "$f[nama_artikel]"; ?></a></h3>
-								 <!-- Go to www.addthis.com/dashboard to customize your tools -->
-									<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-54bc99eb04210058" async="async"></script>
-								<!-- Go to www.addthis.com/dashboard to customize your tools -->
-									<div class="addthis_sharing_toolbox"></div>
-							</div>
-							<img style="max-width: 70%" src="joimg/artikel/<?php echo"$f[gambar]"; ?>" alt="" />
-						</div>
-						<?php echo"$f[isi_artikel]"; ?>	
-					</article>					
-					
 				</div>
 			</div>
 		</div>
