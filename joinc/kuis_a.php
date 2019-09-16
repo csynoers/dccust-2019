4ca20e94ff0d2de6ed01339ba066005d58953ccd
csynoers <3s0c9m7@gmail.com>
2019-09-16 13:21:28 +0700

    kuesinoer

--- joinc/kuis_a.php
+++ joinc/kuis_a.php
@@ -12,6 +12,47 @@
     header('Location:kuis_b.html');
 
   }
+
+	function multiple_radio_button($rows){
+		$html= '';
+		$html .= '
+		<table class="table table-striped table-condensed table-hover">
+			<thead>
+				<tr>
+					<th colspan="4">Tidak sama sekali</th>
+					<th colspan="4">Sangat besar</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td style="width: 45px;"></td>
+					<td style="width: 45px;">1</td>
+					<td style="width: 45px;">2</td>
+					<td style="width: 45px;">3</td>
+					<td style="width: 45px;">4</td>
+					<td style="width: 45px;">5</td>
+					<td></td>
+				</tr>';
+
+				foreach ($rows as $key => $value) {
+					// print_r($value);
+					$html .= '<tr>';
+						$html .= '<td>'.($key+1).'</td>';
+							$html .= '<td><div class="radio"><label><input type="radio" name="tracer_study['.$value->tracer_study_detail_id.']" value="1" required=""></label></div></td>';
+							$html .= '<td><div class="radio"><label><input type="radio" name="tracer_study['.$value->tracer_study_detail_id.']" value="2" required=""></label></div></td>';
+							$html .= '<td><div class="radio"><label><input type="radio" name="tracer_study['.$value->tracer_study_detail_id.']" value="3" required=""></label></div></td>';
+							$html .= '<td><div class="radio"><label><input type="radio" name="tracer_study['.$value->tracer_study_detail_id.']" value="4" required=""></label></div></td>';
+							$html .= '<td><div class="radio"><label><input type="radio" name="tracer_study['.$value->tracer_study_detail_id.']" value="5" required=""></label></div></td>';
+						$html .= '<td>'.strip_tags($value->tracer_study_detail_title).'</td>';
+					$html .= '</tr>';
+				}
+
+		$html .= '
+			</tbody>
+		</table>
+		';
+		return $html;
+	}
 ?>
 <section id="featured">
 	<div class="container">
@@ -25,51 +66,57 @@
 
 				<div class="panel-group" id="accordion">
 					<?php
-						$rows= $db->get_select("SELECT * FROM tracer_studies WHERE tracer_study_parent=0")['data'];
+						$rows= $db->get_select("SELECT *,(SELECT COUNT(t_mod.tracer_study_id) FROM tracer_studies AS t_mod WHERE t_mod.tracer_study_parent=t.tracer_study_id) AS child_count FROM tracer_studies AS t WHERE t.tracer_study_parent=0")['data'];
 						foreach ($rows as $key => $value) {
-							$rows_sub= $db->get_select("SELECT * FROM tracer_studies_title WHERE tracer_study_id='{$value->tracer_study_id}' ")['data'];
-							print_r($rows_sub);
-							// $sub_html= '';
-							// foreach ($rows_sub as $key_sub => $value_sub) {
-							// 	$sub_html .= $value_sub->tarcer_study_detail_title;
-							// }
+							$sub_html = strip_tags($value->tracer_study_desc).'<br>';
+							if ( $value->child_count == 0 ) {
+								$rows_sub= $db->get_select("SELECT * FROM tracer_studies_detail WHERE tracer_study_id='{$value->tracer_study_id}' ")['data'];
+								$sub_html .= multiple_radio_button($rows_sub);
 
-							if ( $key==0 ) {
-								echo '
-									<div class="panel panel-default">
-										<div class="panel-heading">
-											<h4 class="panel-title" style="color: #009a54;">
-												<a data-toggle="collapse" data-parent="#accordion" href="#collapse'.$value->tracer_study_id.'">'.strip_tags($value->tracer_study_title).'</a>
-											</h4>
-										</div>
-										<div id="collapse'.$value->tracer_study_id.'" class="panel-collapse collapse in">
-											<div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-											sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-											minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-											commodo consequat.</div>
-										</div>
-									</div>
-								';
-								
 							} else {
-								echo '
-									<div class="panel panel-default">
-										<div class="panel-heading">
-											<h4 class="panel-title" style="color: #009a54;">
-												<a data-toggle="collapse" data-parent="#accordion" href="#collapse'.$value->tracer_study_id.'">'.strip_tags($value->tracer_study_title).'</a>
-											</h4>
-										</div>
-										<div id="collapse'.$value->tracer_study_id.'" class="panel-collapse collapse">
-											<div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-											sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-											minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-											commodo consequat.</div>
+								$rows_child= $db->get_select("SELECT * FROM tracer_studies WHERE tracer_study_parent='{$value->tracer_study_id}' ")['data'];
+								foreach ($rows_child as $key_rows_child => $value_rows_child) {
+									$rows_sub_child_html = strip_tags($value_rows_child->tracer_study_desc).'<br>';
+									$rows_child_sub = $db->get_select("SELECT * FROM tracer_studies_detail WHERE tracer_study_id='{$value_rows_child->tracer_study_id}' ")['data'];
+									$rows_sub_child_html .= multiple_radio_button($rows_child_sub);
+									$sub_html .= '
+									<div class="col-sm-12">
+										<div class="panel panel-default">
+											<div class="panel-heading">
+												<h4 class="panel-title" style="color: #009a54;">
+													<a data-toggle="collapse" data-parent="#accordion" href="#collapse'.$value_rows_child->tracer_study_id.'">'.strip_tags($value_rows_child->tracer_study_title).'</a>
+												</h4>
+											</div>
+											<div id="collapse'.$value_rows_child->tracer_study_id.'" class="panel-collapse collapse in">
+												<div class="panel-body">
+													'.$rows_sub_child_html.'
+												</div>
+											</div>
 										</div>
 									</div>
-								
-								';
-								
+									';
+
+								}
+								// $sub_html .=
+
 							}
+							
+
+							echo '
+								<div class="panel panel-default">
+									<div class="panel-heading">
+										<h4 class="panel-title" style="color: #009a54;">
+											<a data-toggle="collapse" data-parent="#accordion" href="#collapse'.$value->tracer_study_id.'">'.strip_tags($value->tracer_study_title).'</a>
+										</h4>
+									</div>
+									<div id="collapse'.$value->tracer_study_id.'" class="panel-collapse collapse in">
+										<div class="panel-body">
+											'.$sub_html.'
+										</div>
+									</div>
+								</div>
+							
+							';
 							
 						}
 					?>
